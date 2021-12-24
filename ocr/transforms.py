87d56d5cbffbcc7248c2b4adb482a89d3d0b76e0
Stanislav Kalinin <kalinin@sent.com>
2021-12-24 13:43:59 +0300

    feat: Add random crop and rotate transforms

--- ocr/transforms.py
+++ ocr/transforms.py
@@ -1,4 +1,5 @@
 import torch
+import math
 import torchvision
 import cv2
 import random
@@ -80,6 +81,119 @@ class RandomGaussianBlur:
         return blured_image
 
 
+def img_crop(img, bbox):
+    return img[bbox[1]:bbox[3], bbox[0]:bbox[2]]
+
+
+def random_crop(img, size):
+    tw = size[0]
+    th = size[1]
+    h, w = img.shape[:2]
+    if ((w - tw) > 0) and ((h - th) > 0):
+        x1 = random.randint(0, w - tw)
+        y1 = random.randint(0, h - th)
+    else:
+        x1 = 0
+        y1 = 0
+    img_return = img_crop(img, (x1, y1, x1 + tw, y1 + th))
+    return img_return, x1, y1
+
+
+class RandomCrop:
+    def __init__(self, rnd_crop_min, rnd_crop_max=1):
+        self.factor_max = rnd_crop_max
+        self.factor_min = rnd_crop_min
+
+    def __call__(self, img):
+        factor = random.uniform(self.factor_min, self.factor_max)
+        size = (
+            int(img.shape[1]*factor),
+            int(img.shape[0]*factor)
+        )
+        img, x1, y1 = random_crop(img, size)
+        return img
+
+
+def largest_rotated_rect(w, h, angle):
+    """
+    https://stackoverflow.com/a/16770343
+    Given a rectangle of size wxh that has been rotated by 'angle' (in
+    radians), computes the width and height of the largest possible
+    axis-aligned rectangle within the rotated rectangle.
+    Original JS code by 'Andri' and Magnus Hoff from Stack Overflow
+    Converted to Python by Aaron Snoswell
+    """
+
+    quadrant = int(math.floor(angle / (math.pi / 2))) & 3
+    sign_alpha = angle if ((quadrant & 1) == 0) else math.pi - angle
+    alpha = (sign_alpha % math.pi + math.pi) % math.pi
+
+    bb_w = w * math.cos(alpha) + h * math.sin(alpha)
+    bb_h = w * math.sin(alpha) + h * math.cos(alpha)
+
+    gamma = math.atan2(bb_w, bb_w) if (w < h) else math.atan2(bb_w, bb_w)
+
+    delta = math.pi - alpha - gamma
+
+    length = h if (w < h) else w
+
+    d = length * math.cos(alpha)
+    a = d * math.sin(alpha) / math.sin(delta)
+
+    y = a * math.cos(gamma)
+    x = y * math.tan(gamma)
+
+    return (
+        bb_w - 2 * x,
+        bb_h - 2 * y
+    )
+
+
+def crop_around_center(image, width, height):
+    """
+    https://stackoverflow.com/a/16770343
+    Given a NumPy / OpenCV 2 image, crops it to the given width and height,
+    around it's centre point
+    """
+
+    image_size = (image.shape[1], image.shape[0])
+    image_center = (int(image_size[0] * 0.5), int(image_size[1] * 0.5))
+
+    if(width > image_size[0]):
+        width = image_size[0]
+
+    if(height > image_size[1]):
+        height = image_size[1]
+
+    x1 = int(image_center[0] - width * 0.5)
+    x2 = int(image_center[0] + width * 0.5)
+    y1 = int(image_center[1] - height * 0.5)
+    y2 = int(image_center[1] + height * 0.5)
+
+    return image[y1:y2, x1:x2]
+
+
+class RandomRotate:
+    """Random image rotate around the image center
+    Args:
+        max_ang (float): Max angle of rotation in deg
+    """
+
+    def __init__(self, max_ang=0):
+        self.max_ang = max_ang
+
+    def __call__(self, img):
+        h, w, _ = img.shape
+
+        ang = np.random.uniform(-self.max_ang, self.max_ang)
+        M = cv2.getRotationMatrix2D((w/2, h/2), ang, 1)
+        img = cv2.warpAffine(img, M, (w, h))
+
+        w_cropped, h_cropped = largest_rotated_rect(w, h, math.radians(ang))
+        img = crop_around_center(img, w_cropped, h_cropped)
+        return img
+
+
 class InferenceTransform:
     def __init__(self, height, width):
         self.transforms = get_val_transforms(height, width)
@@ -95,8 +209,10 @@ class InferenceTransform:
 
 def get_train_transforms(height, width, prob):
     transforms = torchvision.transforms.Compose([
+        UseWithProb(RandomGaussianBlur(max_ksize=7), prob=prob),
+        UseWithProb(RandomRotate(5), prob),
+        UseWithProb(RandomCrop(rnd_crop_min=0.85), prob),
         RescalePaddingImage(height, width),
-        UseWithProb(RandomGaussianBlur(max_ksize=3), prob=prob),
         MoveChannels(to_channels_first=True),
         Normalize(),
         ToTensor()
