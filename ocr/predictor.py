6fb77c996f606acc26d95f895039d272ecc85bd0
Stanislav Kalinin <kalinin@sent.com>
2022-03-10 14:21:44 +0300

    docs: Update docstring for OCR predictor

--- ocr/predictor.py
+++ ocr/predictor.py
@@ -27,6 +27,14 @@ def predict(images, model, tokenizer, device):
 
 class OcrPredictor:
     def __init__(self, model_path, config_path, device='cuda'):
+        """Make OCR prediction.
+
+        Args:
+            model_path (str): The path to the model weights.
+            config_path (str): The path to the model config.
+            device (str): The device for computation. Default is cuda.
+        """
+
         config = Config(config_path)
         self.tokenizer = Tokenizer(config.get('alphabet'))
         self.device = torch.device(device)
@@ -44,8 +52,7 @@ class OcrPredictor:
         )
 
     def __call__(self, images):
-        """Make OCR prediction.
-
+        """
         Args:
             images (np.ndarray or list of np.ndarray): One image or list of
                 images in BGR format.
@@ -54,7 +61,6 @@ class OcrPredictor:
             pred (str or list of strs): The predicted text for one input
                 image, and a list with texts if there is a list of input images.
         """
-
         if isinstance(images, (list, tuple)):
             one_image = False
         elif isinstance(images, np.ndarray):
